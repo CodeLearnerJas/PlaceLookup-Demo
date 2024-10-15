6f14ca9bd053c97f3a047b323b53099908608c60
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-10-16 10:41:02 +1100

    Initial Commit

---
+++ PlaceLookup Demo/PlaceLookup_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookup_DemoApp.swift
+//  PlaceLookup Demo
+//
+//  Created by GuitarLearnerJas on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookup_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlaceLookup DemoTests/PlaceLookup_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookup_DemoTests.swift
+//  PlaceLookup DemoTests
+//
+//  Created by GuitarLearnerJas on 16/10/2024.
+//
+
+import Testing
+@testable import PlaceLookup_Demo
+
+struct PlaceLookup_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
